72b23fce65155b8e9cdc21c60633df5d993533d8
Klaus Förster <openwebcc@github.com>
2016-01-03 17:09:52 +0100

    add rawdata migration scripts for Kaunertal campaigns

---
+++ maintenance/rawdata/als/musicals/101007_kaunertal01.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# Datenmigration: Befliegung Kaunertal 10.10.2010, 11.10.2010 und 12.10.2010
+#
+
+# Ordnerstruktur erstellen
+mkdir -pv /home/laser/rawdata/als/musicals/101007_kaunertal01/{asc,las,bet,doc,meta,fix}
+
+# Rohdaten und Dokumentation kopieren
+cd /home/laser/rawdata/als/musicals/101007_kaunertal01
+cp -avu /mnt/netappa/Rohdaten/P7160_MUSICALS/2010/1000-002_nichtkorrigierteDaten/Kaunertal/Flugpfade/*221.bet ./bet/
+cp -avu /mnt/netappa/Rohdaten/P7160_MUSICALS/2010/TopScanBefliegungsbericht_MUSICALS_2010.pdf ./doc/report.pdf
+
+# LAS Files migrieren
+cd /mnt/netappa/Rohdaten/P7160_MUSICALS/2010/1000-002_nichtkorrigierteDaten/Kaunertal/Laserpunkte_nach_Feingeoreferenzierung/Utm/las
+for LAS in `ls *.las`
+do
+    echo "erzeuge /home/laser/rawdata/als/musicals/101007_kaunertal01/las/$LAS ..."
+
+    # Offset entfernen und x,y,z skalieren
+    las2las -i $LAS \
+            -o /home/laser/rawdata/als/musicals/101007_kaunertal01/las/$LAS \
+            -reoffset 0 0 0 \
+            -rescale 0.01 0.01 0.01
+
+    # System identifier und generating software wie beim Original setzen
+    lasinfo -i /home/laser/rawdata/als/musicals/101007_kaunertal01/las/$LAS \
+            -set_system_identifier "ALTM Gemini" \
+            -set_generating_software "OptechLMS" > /dev/null 2>&1
+done
+
+# Jahr und Tag im Jahr setzen
+cd /home/laser/rawdata/als/musicals/101007_kaunertal01/las
+find . -name "*101007*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 280 2010 \;
+find . -name "*101008*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 281 2010 \;
+find . -name "*101009*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 282 2010 \;
+find . -name "*101010*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 283 2010 \;
+find . -name "*101011*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 284 2010 \;
+find . -name "*101012*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 285 2010 \;
+
+# Koordinaten der Trajektorie(n) bereinigen
+for BET in `find /home/laser/rawdata/als/musicals/101007_kaunertal01/bet -name *.bet`
+do
+    echo "entferne 32 bei x-Koordinaten in $BET ..."
+    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
+    mv $BET.xxx $BET
+done
---
+++ maintenance/rawdata/als/musicals/110420_kaunertal02.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# Datenmigration: Befliegung Kaunertal 20.04.2011, 21.04.2011, 22.04.2011 und 23.04.2011
+#
+
+# Ordnerstruktur erstellen
+mkdir -pv /home/laser/rawdata/als/musicals/110420_kaunertal02/{asc,las,bet,doc,meta,fix}
+
+# Rohdaten und Dokumentation kopieren
+cd /home/laser/rawdata/als/musicals/110420_kaunertal02
+cp -avu /mnt/netappa/Rohdaten/P7160_MUSICALS/2011/160-051/Flugpfade/*_221.bet ./bet/
+cp -avu /mnt/netappa/Rohdaten/P7160_MUSICALS/2011/TopScanBefliegungsbericht_MUSICALS_2011.pdf ./doc/report.pdf
+
+# LAS Files migrieren
+cd /mnt/netappa/Rohdaten/P7160_MUSICALS/2011/Laserpunkte_nach_Feingeoreferenzierung/Utm/Las
+for LAS in `ls *.las`
+do
+    echo "erzeuge /home/laser/rawdata/als/musicals/110420_kaunertal02/las/$LAS ..."
+
+    # Offset entfernen und x,y,z skalieren
+    las2las -i $LAS \
+            -o /home/laser/rawdata/als/musicals/110420_kaunertal02/las/$LAS \
+            -reoffset 0 0 0 \
+            -rescale 0.01 0.01 0.01
+
+    # System identifier und generating software wie beim Original setzen
+    lasinfo -i /home/laser/rawdata/als/musicals/110420_kaunertal02/las/$LAS \
+            -set_system_identifier "ALTM Gemini" \
+            -set_generating_software "OptechLMS" > /dev/null 2>&1
+done
+
+# Jahr und Tag im Jahr setzen
+cd /home/laser/rawdata/als/musicals/110420_kaunertal02/las
+find . -name "*110420*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 110 2011 \;
+find . -name "*110421*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 111 2011 \;
+find . -name "*110422*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 112 2011 \;
+find . -name "*110423*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 113 2011 \;
+
+# Koordinaten der Trajektorie(n) bereinigen
+for BET in `find /home/laser/rawdata/als/musicals/110420_kaunertal02/bet -name *.bet`
+do
+    echo "entferne 32 bei x-Koordinaten in $BET ..."
+    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
+    mv $BET.xxx $BET
+done
