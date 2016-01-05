390bc38b9c795024e30dfcaeb9b02efe5c777239
Klaus Förster <openwebcc@github.com>
2016-01-05 17:00:22 +0100

    include LAS-Index files while copying rawdata for VOGIS 2011

--- maintenance/rawdata/als/vogis/111018_vb11.sh
+++ maintenance/rawdata/als/vogis/111018_vb11.sh
@@ -8,7 +8,7 @@ mkdir -pv /home/laser/rawdata/als/vogis/111018_vb11/{asc,las,bet,doc,meta}
 
 # Rohdaten im .las und .laz Format kopieren
 cd /home/laser/rawdata/als/vogis/111018_vb11/las
-cp -avu /mnt/netappa/Rohdaten/vorarlberg_neu_2011/Gelaendemodelle/Lidarpunkte/Kacheln/*.la[sz] .
+cp -avu /mnt/netappa/Rohdaten/vorarlberg_neu_2011/Gelaendemodelle/Lidarpunkte/Kacheln/*.la[szx] .
 
 # Datum auf letztes gefundenes Datum in den Rohdaten der 1. Lieferung setzen (siehe /mnt/netappa/Rohdaten/VoGIS_2011/laz/)
 lasinfo -i *.laz -no_check -quiet -set_file_creation 291 2011
