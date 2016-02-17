367215848060d2388b7f84afbd98e1028bd8bef2
klaus <klaus@geo1.uibk.ac.at>
2016-02-17 12:52:18 +0100

    set new location for data download directory

--- www/lib/Laser/Util/web.py
+++ www/lib/Laser/Util/web.py
@@ -7,8 +7,8 @@ import os
 import re
 
 POINTS_LIMIT = pow(2,32) - 1 # maximum number of points for LAS 1.0/1.1/1.2/1.3
-DOWNLOAD_DIR = '/home/laser/download'
 RAWDATA_DIR = '/home/laser/rawdata'
+DOWNLOAD_DIR = '/home/laser/rawdata/download'
 
 class impl:
     def __init__(self, base=None):
