f174e0a3da882e14e990d169a6cf75652b91dfb7
Klaus Förster <klaus.foerster@uibk.ac.at>
2016-06-01 16:33:23 +0200

    add one more einzelprojekt and fix subdir creation statements

--- maintenance/rawdata/tls/einzelprojekte/migrate_part1.sh
+++ maintenance/rawdata/tls/einzelprojekte/migrate_part1.sh
@@ -4,23 +4,23 @@
 #
 
 # Ordner für Rohdaten erstellen
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/060530_analysis_targets/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/060630_lambert_test/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/060713_zaberbach/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/060726_hochjochhospiz/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/060928_eisriesenwelt/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/070602_dschunglbuch/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/081011_tls_halltal/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/091013_experiment_ipf/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/091013_tls_vorarlberg/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/110323_rangetest_ilris/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/111110_fotsch/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/120919_usb_stick/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/120920_mieming/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/121003_krumgampen/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/130503_adelschlag_tests/raw
-mkdir -p /home/laser/rawdata/tls/einzelprojekte/140404_stubai_phenosat/raw
-
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/060530_analysis_targets
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/060630_lambert_test
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/060713_zaberbach
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/060726_hochjochhospiz
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/060928_eisriesenwelt
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/070602_dschunglbuch
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/081011_tls_halltal
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/091013_experiment_ipf
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/091013_tls_vorarlberg
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/110323_rangetest_ilris
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/111110_fotsch
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/120919_usb_stick
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/120920_mieming
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/121003_krumgampen
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/130503_adelschlag_tests
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/140404_stubai_phenosat
+mkdir -p /home/laser/rawdata/tls/einzelprojekte/140804_schrankogel
 
 # Rohdaten verschieben
 mv /mnt/netappa/Laser/tls/daten/060530_analysis_targets /home/laser/rawdata/tls/einzelprojekte/060530_analysis_targets/raw
@@ -39,3 +39,5 @@ mv /mnt/netappa/Laser/tls/daten/120920_mieming /home/laser/rawdata/tls/einzelpro
 mv /mnt/netappa/Laser/tls/daten/121003_krumgampen /home/laser/rawdata/tls/einzelprojekte/121003_krumgampen/raw
 mv /mnt/netappa/Laser/tls/daten/130503_Adelschlag_Tests /home/laser/rawdata/tls/einzelprojekte/130503_adelschlag_tests/raw
 mv /mnt/netappa/Laser/tls/daten/140404_stubai_phenosat /home/laser/rawdata/tls/einzelprojekte/140404_stubai_phenosat/raw
+mv /mnt/netappa/Laser/tls/daten/140804_schrankogel /home/laser/rawdata/tls/einzelprojekte/140804_schrankogel/raw
+
