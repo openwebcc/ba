d5a9160af67357b5a44c31c816119a42702e9361
Klaus Förster <klaus.foerster@uibk.ac.at>
2016-06-02 13:42:32 +0200

    added Schmirntal Reissenschuh TLS migration script

---
+++ maintenance/rawdata/tls/schmirntal/100531_reissenschuh01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Datenmigration: TLS Kampagne Schmirntal / Reissenschuh - Messung 1, 31.05.2010
+#
+
+# Ordnerstruktur erstellen
+mkdir -pv /home/laser/rawdata/tls/schmirntal/100531_reissenschuh01/{asc,las,doc,meta}
+
+# Rohdaten verschieben
+mv /mnt/netappa/Laser/tls/daten/160531_reissenschuh /home/laser/rawdata/tls/schmirntal/100531_reissenschuh01/raw
