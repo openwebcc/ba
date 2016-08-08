f17b030fc334e081f45006c9c8dd8dc336c3f211
Klaus Förster <openwebcc@github.com>
2016-08-08 16:07:48 +0200

    update hef14 maintenance script after completing migration - use new script to clean up return numbers

--- maintenance/rawdata/als/hef/071011_hef14.sh
+++ maintenance/rawdata/als/hef/071011_hef14.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 #
-# Datenmigration: Befliegung Hintereisferner, 11.10.2007
+# Hintereisferner, 11.10.2007
 #
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/hef/071011_hef14/{asc,las,bet,doc,meta,fix}
+BASE=/home/laser/rawdata/als/hef/071011_hef14
 
-# Rohdaten und Dokumentation kopieren
-cd /home/laser/rawdata/als/hef/071011_hef14
-cp -avu /mnt/netappa/Rohdaten/hef/hef14_071011/str/ala/*.ala ./fix/
-cp -avu /mnt/netappa/Rohdaten/hef/hef14_071011/str/H_071011.bet ./bet/
-cp -avu /mnt/netappa/Rohdaten/hef/hef14_071011/str/h_071011.dgn ./bet/
-cp -avu /mnt/netappa/Rohdaten/hef/hef14_071011/Befliegungsbericht_11102007.pdf ./doc/report.pdf
-
-# ASCII Rohdaten bereinigen und 
-cd /home/laser/rawdata/als/hef/071011_hef14/fix
-for ALA in `ls *.ala`
+cd $BASE/raw/bad/ala
+for GZ in `ls *.ala.gz`
 do
-    echo "strip32 and clean $ALA ..."
-    awk '{gsub(/^32/,"",$2); print}' $ALA > $ALA.tmp
-    mv $ALA.tmp $ALA
+    FNAME=`echo $GZ | sed s/.ala.gz//`
 
+    # set paths to files
+    ALA=`echo $BASE/raw/str/ala/$FNAME.ala`
+    LAS=`echo $BASE/las/$FNAME.las`
+
+    # temporarily uncompress .ala.gz file and remove leading 32 from x-coordinates
+    echo "creating $ALA.tmp (temporary) ..."
+    gunzip -c $GZ | awk '{gsub(/^32/,"",$2); print}' > $ALA.tmp
+
+    # clean return number syntax in .ala file with a python script
+    echo "cleaning $ALA ..."
+    python /home/laser/rawdata/maintenance/rawdata/als/hef/071011_hef14_fix_ala.py --ala=$ALA.tmp --out=$ALA
+
+    # remove temporarily uncompressed .ala.gz file
+    rm $ALA.tmp
+
+    echo "creating $LAS ..."
+
+    # convert to LAS
+    txt2las -i $ALA \
+            -o $LAS \
+            -iparse txyzirn \
+            -reoffset 0 0 0 \
+            -rescale 0.01 0.01 0.01 \
+            -epsg 25832 \
+            -set_file_creation 284 2007 \
+            -set_system_identifier "ALTM 3100"
+
+    # create lasindex
+    lasindex -i $LAS 2>>/dev/null
+
+    # compress .ala file
+    echo "creating $ALA.gz ..."
+    gzip -f $ALA
+
+    break
 done
 
-# Bereinigen von .ala
-# .ala Files haben fast alle 2 2 bei Echo, vereinzelt auch 1 2 und nie 1 1 
-# auf den CDs gibt es keine .ala files, allerdings auch keine .alf und .all files mit Zeitstempeln
-# Datenherkunft kann damit nicht eindeutig gekärt werden
-#
-python /home/klaus/private/ba/maintenance/rawdata/hef/fix_ala_hef14.py
-rename -f 's/\.ala$/.ala.org/' *.ala
-
-# nach LAS konvertieren
-txt2las -i /home/laser/rawdata/als/hef/071011_hef14/asc/*.ala \
-        -odir /home/laser/rawdata/als/hef/071011_hef14/las \
-        -iparse txyzirn \
-        -reoffset 0 0 0 \
-        -rescale 0.01 0.01 0.01 \
-        -epsg 25832 \
-        -set_file_creation 284 2007 \
-        -set_system_identifier "ALTM 3100"
-
-# Koordinaten der Trajektorie(n) bereinigen
-for BET in `find /home/laser/rawdata/als/hef/071011_hef14/bet/ -name *.bet`
+# copy cleaned trajectories
+cd $BASE/raw/str/bet
+for BET in `ls *.bet`
 do
-    echo "entferne 32 bei x-Koordinaten in $BET ..."
-    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
-    mv $BET.xxx $BET
+    echo "creating $BASE/bet/$BET ..."
+    cat $BET | awk '{gsub(/^32/,"",$2); print}' > $BASE/bet/$BET
 done
