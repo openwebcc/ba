7b6256b68a0998933e5817e21cde9d694bdd012f
Klaus Förster <openwebcc@github.com>
2016-08-08 15:57:21 +0200

    enhance python script that cleans up incorrect return number syntax in hef14 .ala files

--- maintenance/rawdata/als/hef/071011_hef14_fix_ala.py
+++ maintenance/rawdata/als/hef/071011_hef14_fix_ala.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 #
-# fix incorrect syntax for echoes in .ala files of 071011_hef14/fix
+# fix incorrect syntax for echoes in .ala files of 071011_hef14
 #
 
+import re
 import os
 import sys
 
@@ -11,59 +12,68 @@ from Laser.Util import las
 
 if __name__ == '__main__':
     """ fix incorrect return number and number of returns for given pulse syntax """
+    import argparse
+    parser = argparse.ArgumentParser(description='fix incorrect syntax for echoes in .ala files')
+    parser.add_argument('--ala', dest='ala', required=True, help='path to input .ala file')
+    parser.add_argument('--out', dest='out', required=True, help='path to cleaned output file')
+    args = parser.parse_args()
+
+    # init utility library
     util = las.rawdata()
-    basedir = '/home/laser/rawdata/als/hef/071011_hef14'
 
+    # open output file
+    o = open(args.out,'w')
 
-    for fname in os.listdir('%s/fix/' % basedir):
-        if not fname[-3:] == 'ala':
-            continue
+    # loop through input file, read pairs of line and clean up
+    with open(args.ala) as f:
+        prev_line = None
+        curr_line = None
+        for line in f:
+            if not prev_line:
+                prev_line = util.parse_line(line)
+                continue
+            else:
+                curr_line = util.parse_line(line)
 
-        # define lower case  .ala output file and open it for writing
-        oname = ('%s.ala' % fname[:-4]).lower()
-        print "creating %s/asc/%s ..." % (basedir,oname)
-        o = open('%s/asc/%s' % (basedir,oname),'w')
+                # alter previous and current line in one turn if gpstimes are the same
+                if prev_line[0] == curr_line[0]:
+                    # set return numbers of previous echo
+                    prev_line[-2] = '1'
+                    prev_line[-1] = '2'
 
-        # loop through input file and read pairs of line
-        with open('%s/fix/%s' % (basedir,fname)) as f:
-            prev_line = None
-            curr_line = None
-            for line in f:
-                if not prev_line:
-                    prev_line = util.parse_line(line)
+                    # set return numbers of current echo
+                    curr_line[-2] = '2'
+                    curr_line[-1] = '2'
+
+                    # write out both lines
+                    o.write('%s\n' % ' '.join(prev_line))
+                    o.write('%s\n' % ' '.join(curr_line))
+
+                    # set previous line to None
+                    prev_line = None
                     continue
                 else:
-                    curr_line = util.parse_line(line)
+                    # write previous line with 1 1 as no second echo is present
+                    prev_line[-2] = '1'
+                    prev_line[-1] = '1'
+                    o.write('%s\n' % ' '.join(prev_line))
 
-                    # alter previous and current line in one turn if gpstimes are the same
-                    if prev_line[0] == curr_line[0]:
-                        # set return numbers of previous echo
-                        prev_line[-2] = '1'
-                        prev_line[-1] = '2'
+                # assign current line as next previous line
+                prev_line = curr_line[:]
 
-                        # set return numbers of current echo
-                        curr_line[-2] = '2'
-                        curr_line[-1] = '2'
+    # write last record from loop if any
+    if prev_line:
+        o.write('%s\n' % ' '.join(prev_line))
 
-                        # write out both lines
-                        o.write('%s\n' % ' '.join(prev_line))
-                        o.write('%s\n' % ' '.join(curr_line))
 
-                        # set previous line to None
-                        prev_line = None
-                        continue
-                    else:
-                        # write previous line with 1 1 as no second echo is present
-                        prev_line[-2] = '1'
-                        prev_line[-1] = '1'
-                        o.write('%s\n' % ' '.join(prev_line))
+    # create log file
+    with open("%s.txt" % args.out, "w") as log:
+        log.write("the corresponding file was created with %s\n" % __file__)
+        log.write("it contains fixed return numbers for first and second returns\n")
+        log.write("\n")
+        log.write("input file with incorrect return numbers: %s\n" % re.sub("raw/str/ala","raw/bad/ala",args.ala[:-4]) )
+        log.write("output file with correct return numbers:  %s\n" % args.out)
+        log.write("\n")
 
-                    # assign current line as next previous line
-                    prev_line = curr_line[:]
-
-        # write last record from loop if any
-        if prev_line:
-            o.write('%s\n' % ' '.join(prev_line))
-
-        # close cleaned output file
-        o.close()
+    # close cleaned output file
+    o.close()
