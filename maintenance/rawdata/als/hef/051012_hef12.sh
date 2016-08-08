3e697ba502627f46efb9264a2a6485a62e34dcb6
Klaus Förster <openwebcc@github.com>
2016-08-08 13:42:36 +0200

    update hef12 maintenance script after completing migration - use new merge script for first, last echoes

--- maintenance/rawdata/als/hef/051012_hef12.sh
+++ maintenance/rawdata/als/hef/051012_hef12.sh
@@ -1,46 +1,59 @@
 #!/bin/bash
 #
-# Datenmigration: Befliegung Hintereisferner, 12.10.2005
+# Hintereisferner, 12.10.2005
 #
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/hef/051012_hef12/{asc,las,bet,doc,meta,fix}
+BASE=/home/laser/rawdata/als/hef/051012_hef12
 
-# Rohdaten und Dokumentation kopieren
-cd /home/laser/rawdata/als/hef/051012_hef12
-cp -avu /mnt/netappa/Rohdaten/hef/hef12_051012/str/alf/*.alf ./fix/
-cp -avu /mnt/netappa/Rohdaten/hef/hef12_051012/str/all/*.all ./fix/
-cp -avu /mnt/netappa/Rohdaten/hef/hef12_051012/h_051012.bet ./bet/
-cp -avu /mnt/netappa/Rohdaten/hef/hef12_051012/Befliegungsbericht\ Oktober\ 2005\ \(Scan\).pdf ./doc/report.pdf
-
-# ASCII Rohdaten bereinigen
-cd /home/laser/rawdata/als/hef/051012_hef12/fix/
-rename -f 'y/A-Z/a-z/' *.all
-for ALFL in `ls *.al[fl]`
+cd $BASE/raw/str/alf
+for GZ in `ls *.alf.gz`
 do
-    echo "strip32 and clean $ALFL ..."
-    awk '{gsub(/^32/,"",$2); print}' $ALFL > $ALFL.tmp
-    mv $ALFL.tmp $ALFL
+    FNAME=`echo $GZ | sed s/.alf.gz//`
+
+    # set paths to files
+    ALF=`echo $BASE/raw/str/alf/$FNAME.alf`
+    ALL=`echo $BASE/raw/str/all/$FNAME.all`
+    ALA=`echo $BASE/raw/str/ala/$FNAME.ala`
+    LAS=`echo $BASE/las/$FNAME.las`
+
+    # temporarily uncompress .alf.gz and .all.gz files and remove leading 32 from x-coordinates
+    echo "creating $ALF (temporary) ..."
+    gunzip -c $BASE/raw/str/alf/$FNAME.alf.gz | awk '{gsub(/^32/,"",$2); print}' > $ALF
+    echo "creating $ALL (temporary) ..."
+    gunzip -c $BASE/raw/str/all/$FNAME.all.gz | awk '{gsub(/^32/,"",$2); print}' > $ALL
+
+    # merge .alf and .all files with a python script
+    echo "creating $ALA (temporary) ..."
+    python /home/laser/rawdata/maintenance/scripts/als/merge_first_last.py --dist=0.1 --first=$ALF --last=$ALL --out=$ALA
+
+    # remove temporarily uncompressed .alf.gz and .all.gz files
+    rm $ALF
+    rm $ALL
+
+    echo "creating $LAS ..."
+    # nach LAS konvertieren
+    txt2las -i $ALA \
+            -o $LAS \
+            -iparse txyzirn \
+            -reoffset 0 0 0 \
+            -rescale 0.01 0.01 0.01 \
+            -epsg 25832 \
+            -set_file_creation 285 2005 \
+            -set_system_identifier "ALTM 3100"
+
+    # create lasindex
+    lasindex -i $LAS 2>>/dev/null
+
+    # compress .ala file
+    echo "creating $ALA.gz ..."
+    gzip -f $ALA
 
 done
 
-# Zusammenspielen von .alf und .all zu .ala
-find /home/laser/rawdata/als/hef/051012_hef12/fix -name "*.alf" -exec sh /home/laser/rawdata/maintenance/rawdata/als/hef/fix_merge_alf_all.sh {} \;
-
-# nach LAS konvertieren
-txt2las -i /home/laser/rawdata/als/hef/051012_hef12/asc/*.ala \
-        -odir /home/laser/rawdata/als/hef/051012_hef12/las \
-        -iparse txyzirn \
-        -reoffset 0 0 0 \
-        -rescale 0.01 0.01 0.01 \
-        -epsg 25832 \
-        -set_file_creation 285 2005 \
-        -set_system_identifier "ALTM 3100"
-
-# Koordinaten der Trajektorie(n) bereinigen
-for BET in `find /home/laser/rawdata/als/hef/051012_hef12/bet/ -name *.bet`
+# copy cleaned trajectories
+cd $BASE/raw/str/bet
+for BET in `ls *.bet`
 do
-    echo "entferne 32 bei x-Koordinaten in $BET ..."
-    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
-    mv $BET.xxx $BET
+    echo "creating $BASE/bet/$BET ..."
+    cat $BET | awk '{gsub(/^32/,"",$2); print}' > $BASE/bet/$BET
 done
