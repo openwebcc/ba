f797df1f334ec489aa08e99c48cf11c4b46f95f2
Klaus Förster <openwebcc@github.com>
2016-08-09 10:58:31 +0200

    update hef21 maintenance script after completing migration

--- maintenance/rawdata/als/hef/111004_hef21.sh
+++ maintenance/rawdata/als/hef/111004_hef21.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
 #
-# Datenmigration: Befliegung Hintereisferner, 04.10.2011
+# Hintereisferner, 04.10.2011
 #
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/hef/111004_hef21/{asc,las,bet,doc,meta}
+BASE=/home/laser/rawdata/als/hef/111004_hef21
 
-# Rohdaten und Dokumentation kopieren
-cd /home/laser/rawdata/als/hef/111004_hef21
-cp -avu /mnt/netappa/Rohdaten/hef/hef21_111004/str/all/*.all ./asc/
-cp -avu /mnt/netappa/Rohdaten/hef/hef21_111004/str/Bet/111004_2_169.bet ./bet/
-cp -avu /mnt/netappa/Rohdaten/hef/hef21_111004/str/Dgn/Hintereisferner.dgn ./bet/
-cp -avu /mnt/netappa/Rohdaten/hef/hef21_111004/str/Dgn/Hintereisferner.dxf ./bet/
-cp -avu /mnt/netappa/Rohdaten/hef/hef21_111004/str/11-UIA-B02-Abschlussbericht.pdf ./doc/report.pdf
-
-# ASCII Rohdaten bereinigen und nach LAS konvertieren
-cd /home/laser/rawdata/als/hef/111004_hef21/asc/
-rename -f 's/\.all$/.ala/' *.all
-for ALA in `ls *.ala`
+# unpack ASCII rawdata and convert it to LAS
+cd $BASE/raw/str/ala
+for GZ in `ls *.all.gz`
 do
-    echo "strip32 and clean $ALA ..."
-    awk '{gsub(/^32/,"",$2); print}' $ALA > $ALA.tmp
-    mv $ALA.tmp $ALA
+    TMP=`echo $BASE/raw/$GZ | sed s/.all.gz/.ala/`
+    LAS=`echo $BASE/las/$GZ | sed s/.all.gz/.las/`
 
-    LAS=`echo $ALA | sed s/\.[^\.]*$/.las/`
-    echo "creating ../las/$LAS ..."
-    txt2las -i $ALA \
-            -o ../las/$LAS \
+    echo "creating $LAS ..."
+
+    # unpack and remove leading 32 from x-coordinates
+    gunzip -c $GZ | awk '{gsub(/^32/,"",$2); print}' > $TMP
+
+    # convert to LAS
+    txt2las -i $TMP \
+            -o $LAS \
             -parse txyzirn \
             -reoffset 0 0 0 \
             -rescale 0.01 0.01 0.01 \
             -epsg 25832 \
             -set_file_creation 277 2011 \
             -set_system_identifier "ALTM 3100"
+
+    # create lasindex
+    lasindex -i $LAS 2>>/dev/null
+
+    rm -f $TMP
 done
 
-# Koordinaten der Trajektorie(n) bereinigen
-for BET in `find /home/laser/rawdata/als/hef/111004_hef21/bet/ -name *.bet`
+# copy cleaned trajectories
+cd $BASE/raw/str/bet
+for BET in `ls *.bet`
 do
-    echo "entferne 32 bei x-Koordinaten in $BET ..."
-    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
-    mv $BET.xxx $BET
+    echo "creating $BASE/bet/$BET ..."
+    cat $BET | awk '{gsub(/^32/,"",$2); print}' > $BASE/bet/$BET
 done
