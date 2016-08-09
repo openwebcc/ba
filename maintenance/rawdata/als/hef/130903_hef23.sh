4f2cea3d1fa6daa08172ade7561b54d0aaf6f0d2
Klaus Förster <openwebcc@github.com>
2016-08-09 12:27:13 +0200

    update hef23 maintenance script after completing migration

--- maintenance/rawdata/als/hef/130903_hef23.sh
+++ maintenance/rawdata/als/hef/130903_hef23.sh
@@ -1,50 +1,46 @@
 #!/bin/bash
 #
-# Datenmigration: Befliegung Hintereisferner, 03.09.2013
+# Hintereisferner, 03.09.2013
 #
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/hef/130903_hef23/{asc,las,bet,doc,meta}
+BASE=/home/laser/rawdata/als/hef/130903_hef23
 
-# Rohdaten und Dokumentation kopieren
-cd /home/laser/rawdata/als/hef/130903_hef23
-cp -avu /mnt/netappa/Rohdaten/hef/Rofental_2013/Laserpunkte/*.all ./asc/
-cp -avu /mnt/netappa/Rohdaten/hef/Rofental_2013/Metadaten/sbet_130903_1_221.bet ./bet/
-cp -avu /mnt/netappa/Rohdaten/hef/Rofental_2013/Metadaten/sbet_130904_1_221.bet ./bet/
-cp -avu /mnt/netappa/Rohdaten/hef/Rofental_2013/Report/13-UIA-B01-Abschlussbericht.pdf ./doc/report.pdf
-cp -avu /mnt/netappa/Rohdaten/hef/Rofental_2013/Uebersicht/Rofental.dgn ./doc/
-cp -avu /mnt/netappa/Rohdaten/hef/Rofental_2013/Uebersicht/Rofental.dxf ./doc/
-cp -avu /mnt/netappa/Rohdaten/hef/Rofental_2013/Uebersicht/Rofental.pdf ./doc/
-
-# ASCII Rohdaten bereinigen und nach LAS konvertieren
-cd /home/laser/rawdata/als/hef/130903_hef23/asc/
-rename -f 's/\.all$/.ala/' *.all
-for ALA in `ls *.ala`
+# unpack ASCII rawdata and convert it to LAS
+cd $BASE/raw/str/ala
+for GZ in `ls *.all.gz`
 do
-    echo "strip32 and clean $ALA ..."
-    awk '{gsub(/^32/,"",$2); print}' $ALA > $ALA.tmp
-    mv $ALA.tmp $ALA
+    TMP=`echo $BASE/raw/$GZ | sed s/.all.gz/.ala/`
+    LAS=`echo $BASE/las/$GZ | sed s/.all.gz/.las/`
 
-    LAS=`echo $ALA | sed s/\.[^\.]*$/.las/`
-    echo "creating ../las/$LAS ..."
-    txt2las -i $ALA \
-            -o ../las/$LAS \
+    echo "creating $LAS ..."
+
+    # unpack and remove leading 32 from x-coordinates
+    gunzip -c $GZ | awk '{gsub(/^32/,"",$2); print}' > $TMP
+
+    # convert to LAS
+    txt2las -i $TMP \
+            -o $LAS \
             -parse txyzirn \
             -reoffset 0 0 0 \
             -rescale 0.01 0.01 0.01 \
             -epsg 25832 \
             -set_system_identifier "ALTM Gemini"
+
+    # create lasindex
+    lasindex -i $LAS 2>>/dev/null
+
+    rm -f $TMP
 done
 
-# Tag im Jahr und Jahr ergänzen
-cd /home/laser/rawdata/als/hef/130903_hef23/las
+# set day in year and year for LAS files
+cd $BASE/las
 find . -name "*130903*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 246 2013 \;
 find . -name "*130904*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 247 2013 \;
 
-# Koordinaten der Trajektorie(n) bereinigen
-for BET in `find /home/laser/rawdata/als/hef/130903_hef23/bet/ -name *.bet`
+# copy cleaned trajectories
+cd $BASE/raw/str/bet
+for BET in `ls *.bet`
 do
-    echo "entferne 32 bei x-Koordinaten in $BET ..."
-    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
-    mv $BET.xxx $BET
+    echo "creating $BASE/bet/$BET ..."
+    cat $BET | awk '{gsub(/^32/,"",$2); print}' > $BASE/bet/$BET
 done
