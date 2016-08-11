7a8db31dc88dc628558c5e168231a049a5da21ed
Klaus Förster <openwebcc@github.com>
2016-08-11 15:52:24 +0200

    rewrite hef19 data migration script that extracts hef data from musicals 2010 campaign

--- maintenance/rawdata/als/hef/101007_hef19.sh
+++ maintenance/rawdata/als/hef/101007_hef19.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
 #
-# Datenmigration: Befliegung Hintereisferner 07.10.2010, 08.10.2010, 09.10.2010, 10.10.2010, 11.10.2010 und 12.10.2010
-#                 Daten stammen aus der Befliegung musicals/101007_kaunertal01
+# Hintereisferner 07.10.2010, 08.10.2010, 09.10.2010, 10.10.2010, 11.10.2010 und 12.10.2010
+# data is extracted from musicals/101007_kaunertal01
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/hef/101007_hef19/{asc,las,bet,doc,meta}
+BASE=/home/laser/rawdata/als/hef/101007_hef19
+BASE_MUSICALS=/home/laser/rawdata/als/musicals/101007_kaunertal01
+TMPDIR=/tmp/hef19
 
-# Trajektorien und Dokumentation kopieren
-cd /home/laser/rawdata/als/hef/101007_hef19
-cp -avu /home/laser/rawdata/als/musicals/101007_kaunertal01/bet/*.bet ./bet/
-cp -avu /home/laser/rawdata/als/musicals/101007_kaunertal01/doc/*.pdf ./doc/report.pdf
+# copy trajectories and documentation
+cp -avu $BASE_MUSICALS/bet/*.bet $BASE/bet/
+cp -avu $BASE_MUSICALS/doc/report.pdf $BASE/doc/report.pdf
 
-#
-# Rohdaten aus der Befliegung musicals/101007_kaunertal01 extrahieren
-#
-
-# Schritt 1: Extent des HEF-Projektpolygons ermitteln
-OGR_EXT=`ogrinfo /home/laser/rawdata/maintenance/rawdata/als/hef/util/hef_projectpolygon.shp hef_projectpolygon | grep Extent`
+# get extent of HEF project polygon
+OGR_EXT=`ogrinfo /home/laser/rawdata/maintenance/util/aoi/hef_25832.geojson OGRGeoJSON -so | grep Extent`
 KEEP_XY=`echo $OGR_EXT | sed -e 's/[a-zA-Z:,\(\)\-]//g'`
 
-# Schritt 2: Flugstreifen der musicals Befliegung am Extent des HEF-Projektpolygons klippen
-mkdir /tmp/hef19
-cd /home/laser/rawdata/als/musicals/101007_kaunertal01/las
-las2las -i *.las -odir /tmp/hef19 -olas -keep_xy $KEEP_XY
-
-# Schritt 3: leere Flugstreifen löschen
-find /tmp/hef19 -size -350c -exec rm -f {} \;
-
-# Schritt 4: Flugstreifen am Projektpolygon klippen
-find /tmp/hef19 -name "*.las" -exec python /home/laser/rawdata/maintenance/scripts/als/clip_lasfile.py \
-    --lasfile {} \
-    --wktpoly /home/laser/rawdata/maintenance/rawdata/als/hef/util/hef_projectpolygon.wkt \
-    --outdir /home/laser/rawdata/als/hef/101007_hef19/las/ \;
-
-# Schritt 5: system identifier, generating software setzen und kopierten LAS-header aktualisieren
-cd /home/laser/rawdata/als/hef/101007_hef19/las/
+# clip data of musicals campaign on extent of HEF project extent
+mkdir $TMPDIR
+cd $BASE_MUSICALS/las
 for LAS in `ls *.las`
 do
-    lasinfo -i $LAS -set_system_identifier "ALTM Gemini" -set_generating_software "OptechLMS" -repair > /dev/null 2>&1
+    # clip on HEF extent
+    echo "clipping $LAS on extent ..."
+    las2las -i $BASE_MUSICALS/las/$LAS -odir $TMPDIR -olas -keep_xy $KEEP_XY
+
+    # remove empty clipping result indicated by filesize less than 350
+    if [ $(wc -c <"$TMPDIR/$LAS") -le 350 ]; then
+        echo "    no points found within extent"
+    else
+        # clip on HEF project polygon and stor
+        echo "clipping $LAS on project polygon ..."
+        python /home/laser/rawdata/maintenance/scripts/als/clip_lasfile.py \
+            --lasfile $TMPDIR/$LAS \
+            --wktpoly /home/laser/rawdata/maintenance/util/aoi/hef_25832.wkt \
+            --outdir $BASE/las
+
+        # remove temporary file
+        rm -f $TMPDIR/$LAS
+
+        # correct systm LAS attributes
+        lasinfo -i $BASE/las/$LAS -set_system_identifier "ALTM Gemini" -set_generating_software "OptechLMS" -repair > /dev/null 2>&1
+
+        # create lasindex
+        lasindex -i $BASE/las/$LAS 2>>/dev/null
+
+    fi
 done
 
-# Schritt 6: leere Flugstreifen löschen
-find /home/laser/rawdata/als/hef/101007_hef19/las -size -350c -exec rm -f {} \;
+# remove empty data strips as results of clipping on project polygon
+find $BASE/las -size -350c -exec rm -f {} \;
 
-# clean up
-rm -rf /tmp/hef19/
+# remove temporary directory
+rm -rf $TMPDIR/
+
