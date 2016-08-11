b9aec0763c62f33ef8549cfa20d23b1283731694
Klaus Förster <openwebcc@github.com>
2016-08-11 15:55:21 +0200

    use hef19 maintenance script for hef20 as well extracting data from musicals 2011 campaign

--- maintenance/rawdata/als/hef/110420_hef20.sh
+++ maintenance/rawdata/als/hef/110420_hef20.sh
@@ -1,47 +1,54 @@
 #!/bin/bash
 #
-# Datenmigration: Befliegung Hintereisferner 20.04.2011, 21.04.2011, 22.04.2011 und 23.04.2011
-#                 Daten stammen aus der Befliegung musicals/110420_kaunertal02
+# Hintereisferner 20.04.2011, 21.04.2011, 22.04.2011 und 23.04.2011
+# data is extracted from musicals/110420_kaunertal02
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/hef/110420_hef20/{asc,las,bet,doc,meta}
+BASE=/home/laser/rawdata/als/hef/110420_hef20
+BASE_MUSICALS=/home/laser/rawdata/als/musicals/110420_kaunertal02
+TMPDIR=/tmp/hef20
 
-# Trajektorien und Dokumentation kopieren
-cd /home/laser/rawdata/als/hef/110420_hef20
-cp -avu /home/laser/rawdata/als/musicals/110420_kaunertal02/bet/*.bet ./bet/
-cp -avu /home/laser/rawdata/als/musicals/110420_kaunertal02/doc/*.pdf ./doc/report.pdf
+# copy trajectories and documentation
+cp -avu $BASE_MUSICALS/bet/*.bet $BASE/bet/
+cp -avu $BASE_MUSICALS/doc/report.pdf $BASE/doc/report.pdf
 
-#
-# Rohdaten aus der Befliegung musicals/110420_kaunertal02 extrahieren
-#
-
-# Schritt 1: Extent des HEF-Projektpolygons ermitteln
-OGR_EXT=`ogrinfo /home/laser/rawdata/maintenance/rawdata/als/hef/util/hef_projectpolygon.shp hef_projectpolygon | grep Extent`
+# get extent of HEF project polygon
+OGR_EXT=`ogrinfo /home/laser/rawdata/maintenance/util/aoi/hef_25832.geojson OGRGeoJSON -so | grep Extent`
 KEEP_XY=`echo $OGR_EXT | sed -e 's/[a-zA-Z:,\(\)\-]//g'`
 
-# Schritt 2: Flugstreifen der musicals Befliegung am Extent des HEF-Projektpolygons klippen
-mkdir /tmp/hef20
-cd /home/laser/rawdata/als/musicals/110420_kaunertal02/las
-las2las -i *.las -odir /tmp/hef20 -olas -keep_xy $KEEP_XY
-
-# Schritt 3: leere Flugstreifen löschen
-find /tmp/hef20 -size -350c -exec rm -f {} \;
-
-# Schritt 4: Flugstreifen am Projektpolygon klippen
-find /tmp/hef20 -name "*.las" -exec python /home/laser/rawdata/maintenance/scripts/als/clip_lasfile.py \
-    --lasfile {} \
-    --wktpoly /home/laser/rawdata/maintenance/rawdata/als/hef/util/hef_projectpolygon.wkt \
-    --outdir /home/laser/rawdata/als/hef/110420_hef20/las/ \;
-
-# Schritt 5: system identifier, generating software setzen und kopierten LAS-header aktualisieren
-cd /home/laser/rawdata/als/hef/110420_hef20/las/
+# clip data of musicals campaign on extent of HEF project extent
+mkdir $TMPDIR
+cd $BASE_MUSICALS/las
 for LAS in `ls *.las`
 do
-    lasinfo -i $LAS -set_system_identifier "ALTM Gemini" -set_generating_software "OptechLMS" -repair > /dev/null 2>&1
+    # clip on HEF extent
+    echo "clipping $LAS on extent ..."
+    las2las -i $BASE_MUSICALS/las/$LAS -odir $TMPDIR -olas -keep_xy $KEEP_XY
+
+    # remove empty clipping result indicated by filesize less than 350
+    if [ $(wc -c <"$TMPDIR/$LAS") -le 350 ]; then
+        echo "    no points found within extent"
+    else
+        # clip on HEF project polygon and stor
+        echo "clipping $LAS on project polygon ..."
+        python /home/laser/rawdata/maintenance/scripts/als/clip_lasfile.py \
+            --lasfile $TMPDIR/$LAS \
+            --wktpoly /home/laser/rawdata/maintenance/util/aoi/hef_25832.wkt \
+            --outdir $BASE/las
+
+        # remove temporary file
+        rm -f $TMPDIR/$LAS
+
+        # correct systm LAS attributes
+        lasinfo -i $BASE/las/$LAS -set_system_identifier "ALTM Gemini" -set_generating_software "OptechLMS" -repair > /dev/null 2>&1
+
+        # create lasindex
+        lasindex -i $BASE/las/$LAS 2>>/dev/null
+
+    fi
 done
 
-# Schritt 6: leere Flugstreifen löschen
-find /home/laser/rawdata/als/hef/110420_hef20/las -size -350c -exec rm -f {} \;
+# remove empty data strips as results of clipping on project polygon
+find $BASE/las -size -350c -exec rm -f {} \;
 
-# clean up
-rm -rf /tmp/hef20/
+# remove temporary directory
+rm -rf $TMPDIR/
