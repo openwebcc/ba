5ad7a4b2ab9c7f821a6ed54debb7dca1bdfbf433
Klaus Förster <openwebcc@github.com>
2016-08-16 10:22:28 +0200

    fix incorrect variable notation

--- maintenance/create_lasinfo.sh
+++ maintenance/create_lasinfo.sh
@@ -13,12 +13,12 @@ if test "$1" = "rebuild"; then
     REBUILD=rebuild
 fi
 
-find /home/laser/rawdata/als/c4austria -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
-find /home/laser/rawdata/als/engabreen -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
-find /home/laser/rawdata/als/hef -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
-find /home/laser/rawdata/als/montafon -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
-find /home/laser/rawdata/als/musicals -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
-find /home/laser/rawdata/als/schmirntal -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
-find /home/laser/rawdata/als/vinschgau -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
-find /home/laser/rawdata/als/vogis -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
-find /home/laser/rawdata/tls/schmirntal -wholename "*/las/*.la[sz]" -exec /bin/sh SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/als/c4austria -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/als/engabreen -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/als/hef -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/als/montafon -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/als/musicals -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/als/schmirntal -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/als/vinschgau -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/als/vogis -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
+find /home/laser/rawdata/tls/schmirntal -wholename "*/las/*.la[sz]" -exec /bin/sh $SCRIPTDIR/get_lasinfo.sh {} $REBUILD \;
