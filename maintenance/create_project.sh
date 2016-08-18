9d6d148238e9387d4c40a15b0d5c905c3225167f
Klaus Förster <openwebcc@github.com>
2016-08-18 14:38:29 +0200

    add script to create als/tls project directory skeletons

---
+++ maintenance/create_project.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# create project skeleton for als / tls projects
+#
+
+# check input
+if [ -z "$1" ]; then 
+    echo "Usage: sh $0 TYPE SUBDIR NAME"
+    echo "no project type specified (als|tls)"
+    exit
+
+elif [ -z "$2" ]; then 
+    echo "Usage: sh $0 TYPE SUBDIR NAME"
+    echo "no project subdirectory specified (e.g. hef)"
+    exit
+elif [ -z "$3" ]; then 
+    echo "Usage: sh $0 TYPE SUBDIR NAME"
+    echo "no project name specified (e.g. YYMMDD_name)"
+    exit
+else
+    # everything is fine
+    PTYPE=$1
+    PDIR=$2
+    PNAME=$3
+fi
+
+
+# create skeleton
+if test "$PTYPE" = "als"; then
+    # create ALS-data skeleton
+    echo "going als"
+    for DIR in `echo "raw las bet doc meta"`
+    do
+        mkdir -pv /home/laser/rawdata/$PTYPE/$PDIR/$PNAME/$DIR
+    done
+    sh set_perms.sh /home/laser/rawdata/$PTYPE/$PDIR/$PNAME
+
+elif test "$PTYPE" = "tls"; then
+    # create TLS-data skeleton
+    echo "TLS-data skeleton not yet impelemented"
+
+else
+    echo "'$PTYPE' is not a valid project type"
+    echo "Usage: sh $0 TYPE SUBDIR NAME"
+fi
