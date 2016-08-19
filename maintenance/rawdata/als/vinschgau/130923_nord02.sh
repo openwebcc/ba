03a018d622f978c39a352f8bcc9ef0c10b390eb0
Klaus Förster <openwebcc@github.com>
2016-08-19 15:03:25 +0200

    update vinschgau maintenance scripts after completing migration

--- maintenance/rawdata/als/vinschgau/130923_nord02.sh
+++ maintenance/rawdata/als/vinschgau/130923_nord02.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 #
-# Datenmigration: Befliegung Vinschgau Nord, 23.9.2013
+# Vinschgau Nord, 23.9.2013
 #
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/vinschgau/130923_nord02/{asc,las,bet,doc,meta}
+BASE=/home/laser/rawdata/als/vinschgau/130923_nord02
 
-# Rohdaten und Dokumentation kopieren
-cd /home/laser/rawdata/als/vinschgau/130923_nord02
-cp -av /mnt/netappa/Rohdaten/P7160_028_012_VINSCHGLAC/Nord/Laserpunkte/zweites_datum/*.all ./asc/
-cp -av /mnt/netappa/Rohdaten/P7160_028_012_VINSCHGLAC/Nord/Metadaten/130923_1_221.bet ./bet/
-cp -av /mnt/netappa/Rohdaten/P7160_028_012_VINSCHGLAC/Nord/Uebersicht/* ./bet/
-cp -av /mnt/netappa/Rohdaten/P7160_028_012_VINSCHGLAC/Report/13-UIA-B02-Abschlussbericht.pdf ./doc/report.pdf
-
-# ASCII Rohdaten bereinigen
-cd /home/laser/rawdata/als/vinschgau/130923_nord02/asc
-rename -f 's/\.all$/.ala/' *.all
-for ALA in `ls *.ala`
+# unpack ASCII rawdata and convert it to LAS
+cd $BASE/raw/str/ala
+for GZ in `ls *.all.gz`
 do
-    echo "strip32 and clean $ALA ..."
-    awk '{gsub(/^32/,"",$2); print}' $ALA > $ALA.tmp
-    mv $ALA.tmp $ALA
+    TMP=`echo $BASE/raw/$GZ | sed s/.all.gz/.all/`
+    LAS=`echo $BASE/las/$GZ | sed s/.all.gz/.las/`
+
+    echo "creating $LAS ..."
+
+    # unpack and remove leading 32 from x-coordinates
+    gunzip -c $GZ | awk '{gsub(/^32/,"",$2); print}' > $TMP
+
+    # convert to LAS
+    txt2las -i $TMP \
+            -o $LAS \
+            -parse txyzirn \
+            -reoffset 0 0 0 \
+            -rescale 0.01 0.01 0.01 \
+            -epsg 25832 \
+            -set_file_creation 266 2013 \
+            -set_system_identifier "ALTM Gemini"
+
+    # create lasindex
+    lasindex -i $LAS 2>>/dev/null
+
+    rm -f $TMP
 done
 
-# nach LAS konvertieren
-txt2las -i /home/laser/rawdata/als/vinschgau/130923_nord02/asc/*.ala \
-        -odir /home/laser/rawdata/als/vinschgau/130923_nord02/las \
-        -parse txyzirn \
-        -reoffset 0 0 0 \
-        -rescale 0.01 0.01 0.01 \
-        -epsg 25832 \
-        -set_file_creation 266 2013 \
-        -set_system_identifier "ALTM Gemini"
-
-# strip 32 from .bet file(s)
-for BET in `find /home/laser/rawdata/als/vinschgau/130923_nord02/bet/ -name "*.bet"`
+# copy cleaned trajectories
+cd $BASE/raw/str/bet
+for BET in `ls *.bet`
 do
-    echo "entferne 32 bei x-Koordinaten in $BET ..."
-    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
-    mv $BET.xxx $BET
+    echo "creating $BASE/bet/$BET ..."
+    cat $BET | awk '{gsub(/^32/,"",$2); print}' > $BASE/bet/$BET
 done
