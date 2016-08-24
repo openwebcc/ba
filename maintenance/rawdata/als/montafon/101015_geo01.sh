596157cf90de644784deb7d6a0b60a385b2e74b1
Klaus Förster <openwebcc@github.com>
2016-08-24 11:54:42 +0200

    update montafon maintenance scripts after completing migration

--- maintenance/rawdata/als/montafon/101015_geo01.sh
+++ maintenance/rawdata/als/montafon/101015_geo01.sh
@@ -1,51 +1,41 @@
 #!/bin/bash
 #
-# Datenmigration: Befliegung Montafon 13.10.2010, 14.10.2010 und 15.10.2010
+# Montafon 13.10.2010, 14.10.2010, 15.10.2010
 #
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/montafon/101015_geo01/{asc,las,bet,doc,meta}
+BASE=/home/laser/rawdata/als/montafon/101015_geo01
 
-# Rohdaten und Dokumentation kopieren
-cd /home/laser/rawdata/als/montafon/101015_geo01
-mv -v /mnt/netappa/Rohdaten/Montafon/Topscan/Laserpunkte/*-1010*.all.gz ./asc/
-mv -v /mnt/netappa/Rohdaten/Montafon/Topscan/Metadaten/1010*.bet ./bet/
-cp -avu /mnt/netappa/Rohdaten/Montafon/Topscan/Uebersicht/Montafon.* ./bet/
-cp -avu /mnt/netappa/Rohdaten/Montafon/Topscan/Projektbericht/11-UIA-B05-Abschlussbericht.pdf ./doc/report.pdf
-
-
-# ASCII Rohdaten bereinigen
-cd /home/laser/rawdata/als/montafon/101015_geo01/asc
-rename -f 's/\.all\.gz$/.ala.gz/' *.all.gz
-for GZ in `ls *.gz`
+cd $BASE/raw/str/ala
+for GZ in `ls *.all.gz`
 do
-    ALA=`echo $GZ | sed s/\.gz$//`
-    echo "strip32 and clean $ALA ..."
-    zcat $GZ | awk '{gsub(/^32/,"",$2); print}' - > $ALA
+    TMP=`echo $BASE/raw/$GZ | sed s/.all.gz/.all/`
+    LAS=`echo $BASE/las/$GZ | sed s/.all.gz/.las/`
 
+    echo "creating $LAS ..."
+
+    # unpack
+    gunzip -c $GZ > $TMP
+
+    # convert to LAS
+    txt2las -i $TMP \
+            -o $LAS \
+            -parse txyzirn \
+            -reoffset 0 0 0 \
+            -rescale 0.01 0.01 0.01 \
+            -epsg 25832 \
+            -set_system_identifier "ALTM Gemini"
+
+    # create lasindex
+    lasindex -i $LAS 2>>/dev/null
+
+    rm -f $TMP
 done
-rm -f *.ala.gz
 
-# nach LAS konvertieren
-txt2las -i /home/laser/rawdata/als/montafon/101015_geo01/asc/*.ala \
-        -odir /home/laser/rawdata/als/montafon/101015_geo01/las \
-        -parse txyzirn \
-        -reoffset 0 0 0 \
-        -rescale 0.01 0.01 0.01 \
-        -epsg 25832 \
-        -set_system_identifier "ALTM Gemini"
-
-# Jahr und Tag im Jahr setzen
-cd /home/laser/rawdata/als/montafon/101015_geo01/las
+# set day of year and year
+cd $BASE/las
 find . -name "*101013*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 288 2010 \;
 find . -name "*101014*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 289 2010 \;
 find . -name "*101015*.las" -exec lasinfo -i {} -no_check -quiet -set_file_creation 290 2010 \;
 
-# Koordinaten der Trajektorie(n) bereinigen
-for BET in `find /home/laser/rawdata/als/montafon/101015_geo01/bet -name *.bet`
-do
-    echo "entferne 32 bei x-Koordinaten in $BET ..."
-    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
-    mv $BET.xxx $BET
-done
-
+# copy trajectories
+cp -av $BASE/raw/str/bet/*.bet $BASE/bet/
--- maintenance/rawdata/als/montafon/111005_geo02.sh
+++ maintenance/rawdata/als/montafon/111005_geo02.sh
@@ -1,46 +1,36 @@
 #!/bin/bash
 #
-# Datenmigration: Nachflug Montafon 05.10.2011
+# Nachflug Montafon 05.10.2011
 #
 
-# Ordnerstruktur erstellen
-mkdir -pv /home/laser/rawdata/als/montafon/111005_geo02/{asc,las,bet,doc,meta}
+BASE=/home/laser/rawdata/als/montafon/111005_geo02
 
-# Rohdaten und Dokumentation kopieren
-cd /home/laser/rawdata/als/montafon/111005_geo02
-mv -v /mnt/netappa/Rohdaten/Montafon/Topscan/Laserpunkte/*-111005*.all.gz ./asc/
-mv -v /mnt/netappa/Rohdaten/Montafon/Topscan/Metadaten/111005*.bet ./bet/
-mv -v /mnt/netappa/Rohdaten/Montafon/Topscan/Uebersicht/Montafon.* ./bet/
-mv -v /mnt/netappa/Rohdaten/Montafon/Topscan/Projektbericht/11-UIA-B05-Abschlussbericht.pdf ./doc/report.pdf
-
-
-# ASCII Rohdaten bereinigen
-cd /home/laser/rawdata/als/montafon/111005_geo02/asc
-rename -f 's/\.all\.gz$/.ala.gz/' *.all.gz
-for GZ in `ls *.gz`
+cd $BASE/raw/str/ala
+for GZ in `ls *.all.gz`
 do
-    ALA=`echo $GZ | sed s/\.gz$//`
-    echo "strip32 and clean $ALA ..."
-    zcat $GZ | awk '{gsub(/^32/,"",$2); print}' - > $ALA
+    TMP=`echo $BASE/raw/$GZ | sed s/.all.gz/.all/`
+    LAS=`echo $BASE/las/$GZ | sed s/.all.gz/.las/`
 
+    echo "creating $LAS ..."
+
+    # unpack
+    gunzip -c $GZ > $TMP
+
+    # convert to LAS
+    txt2las -i $TMP \
+            -o $LAS \
+            -parse txyzirn \
+            -reoffset 0 0 0 \
+            -rescale 0.01 0.01 0.01 \
+            -epsg 25832 \
+            -set_file_creation 278 2011 \
+            -set_system_identifier "ALTM 3100"
+
+    # create lasindex
+    lasindex -i $LAS 2>>/dev/null
+
+    rm -f $TMP
 done
-rm -f *.ala.gz
 
-# nach LAS konvertieren
-txt2las -i /home/laser/rawdata/als/montafon/111005_geo02/asc/*.ala \
-        -odir /home/laser/rawdata/als/montafon/111005_geo02/las \
-        -parse txyzirn \
-        -reoffset 0 0 0 \
-        -rescale 0.01 0.01 0.01 \
-        -epsg 25832 \
-        -set_file_creation 278 2011 \
-        -set_system_identifier "ALTM 3100"
-
-
-# Koordinaten der Trajektorie(n) bereinigen
-for BET in `find /home/laser/rawdata/als/montafon/111005_geo02/bet -name *.bet`
-do
-    echo "entferne 32 bei x-Koordinaten in $BET ..."
-    awk '{gsub(/^32/,"",$2);print}' $BET > $BET.xxx
-    mv $BET.xxx $BET
-done
+# copy trajectories
+cp -av $BASE/raw/str/bet/*.bet $BASE/bet/
