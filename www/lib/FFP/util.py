6eee6223a9ad3777a3d23c67b1495c9086e6e252
Klaus Förster <openwebcc@github.com>
2016-11-10 14:13:42 +0100

    add base-, download directory and download availability to FFP configuration

--- www/lib/FFP/util.py
+++ www/lib/FFP/util.py
@@ -11,11 +11,26 @@ class Config:
 
     def __init__(self):
         """ define globals """
+        self.base_dir = '/home/laser/rawdata/ffp'
+        self.download_dir = '/home/laser/rawdata/download/ffp'
+        self.download_hours_available = 48
         self.app_root = '/data/ffp'
         self.leaflet_root = '/data/lib'
         self.dataset_title = {
             '090930_gesamt' : 'Gesamtbefliegung 2006 - 2009',
         }
+
+    def get_base_dir(self):
+        """ return full path to download directory """
+        return self.base_dir
+
+    def get_download_dir(self):
+        """ return full path to download directory """
+        return self.download_dir
+
+    def get_download_hours_available(self):
+        """ return number of hours that data will be available """
+        return self.download_hours_available
 
     def get_app_root(self):
         """ return URI to the base directory of the application """
@@ -31,4 +46,3 @@ class Config:
             return self.dataset_title[dataset]
         else:
             return None
-
