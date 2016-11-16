4026fa3b7ffda9cea00e4f9a929dd485c8ada19d
Klaus Förster <klaus.foerster@uibk.ac.at>
2016-11-16 11:17:40 +0100

    add maintenance script for ALS campaign Suldenferner 2016

---
+++ maintenance/rawdata/als/vinschgau/160825_suldenferner.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Suldenferner, 25.8.2016
+#
+
+BASE=/home/laser/rawdata/als/vinschgau/160825_suldenferner
+
+# unpack ASCII rawdata and convert it to LAS
+cd $BASE/raw/str/all
+for GZ in `ls *.all.gz`
+do
+    TMP=`echo $BASE/raw/$GZ | sed s/.all.gz/.all/`
+    LAS=`echo $BASE/las/$GZ | sed s/.all.gz/.las/`
+
+    echo "creating $LAS ..."
+
+    # unpack and remove leading 32 from x-coordinates
+    gunzip -c $GZ | awk '{gsub(/^32/,"",$2); print}' > $TMP
+
+    # convert to LAS
+    txt2las -i $TMP \
+            -o $LAS \
+            -parse txyzirn \
+            -reoffset 0 0 0 \
+            -rescale 0.01 0.01 0.01 \
+            -epsg 25832 \
+            -set_file_creation 238 2016 \
+            -set_system_identifier "UNKNOWN"
+
+    # create lasindex
+    lasindex -i $LAS 2>>/dev/null
+
+    rm -f $TMP
+done
+
+# copy cleaned trajectories
+cd $BASE/raw/str/bet
+for BET in `ls *.bet`
+do
+    echo "creating $BASE/bet/$BET ..."
+    cat $BET | awk '{gsub(/^32/,"",$2); print}' > $BASE/bet/$BET
+done
