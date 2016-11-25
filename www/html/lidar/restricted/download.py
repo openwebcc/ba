124fb8eb4c07de6de830110ac5e162ace231f4d8
Klaus Förster <openwebcc@github.com>
2016-11-25 12:38:37 +0100

    implement logging of user downloads in LiDAR application

--- www/html/lidar/restricted/download.py
+++ www/html/lidar/restricted/download.py
@@ -19,6 +19,12 @@ import Laser.Util.web
 
 APP_ROOT = '/data/lidar'
 
+def _log_files(req,dbh,base,files):
+    """ log files downloaded by user """
+    dbh.execute("INSERT INTO lidar_log (user_id,files) VALUES (%s,%s)", (
+        base.get_user(),files
+    ))
+
 def index(req):
     """ provide empty startpage for now """
 
@@ -29,7 +35,7 @@ def lasfile(req, gid=None):
     """ get lasfile """
 
     # init application
-    (base,dbh,tpl) = Laser.base.impl().init(req)
+    (base,dbh,tpl) = Laser.base.impl().init(req,user='intranet')
     util = Laser.Util.web.impl(base)
 
     dbh.execute("""SELECT ptype,pname,cdate,cname,fname FROM view_lidar_meta WHERE gid=%s""", (gid,))
@@ -38,9 +44,10 @@ def lasfile(req, gid=None):
         ipath = '%s/las/%s' % (util.path_to_campaign(cid),row['fname'])
         opath = '%s/%s_%s' % (util.get_download_dir(),util.get_cid_as_prefix(cid),row['fname'])
 
-        # execute command and give feedback
+        # execute command, log files and give feedback
         proc = Popen(['ln', '-s', ipath, opath], stdout=PIPE, stderr=PIPE)
         stdout, stderr = proc.communicate()
+        _log_files(req,dbh,base,[ipath])
         req.write('<h3>Daten im Downloadbereich bereitgestellt</h3>')
         req.write("<pre><strong>Shell-Befehl:</strong>\n\nln -s %s %s</pre><br>" % (ipath,opath))
 
@@ -53,7 +60,7 @@ def trajectory(req, gid=None):
     """ get trajectory """
 
     # init application
-    (base,dbh,tpl) = Laser.base.impl().init(req)
+    (base,dbh,tpl) = Laser.base.impl().init(req,user='intranet')
     util = Laser.Util.web.impl(base)
 
     dbh.execute("""SELECT ptype,pname,cdate,cname,fname FROM view_lidar_meta WHERE gid=%s""", (gid,))
@@ -63,9 +70,10 @@ def trajectory(req, gid=None):
         opath = '%s/%s_%s.traj.txt' % (util.get_download_dir(),util.get_cid_as_prefix(cid),row['fname'])
 
         if os.path.exists(ipath):
-            # execute command and give feedback
+            # execute command, log files and give feedback
             proc = Popen(['ln', '-s', ipath, opath], stdout=PIPE, stderr=PIPE)
             stdout, stderr = proc.communicate()
+            _log_files(req,dbh,base,[ipath])
             req.write('<h3>Daten im Downloadbereich bereitgestellt</h3>')
             req.write("<pre><strong>Shell-Befehl:</strong>\n\nln -s %s %s</pre><br>" % (ipath,opath))
         else:
@@ -80,7 +88,7 @@ def points(req, cid=None, extent=None):
     """ get points for campaign within extent from strips intersecting extent """
 
     # init application
-    (base,dbh,tpl) = Laser.base.impl().init(req)
+    (base,dbh,tpl) = Laser.base.impl().init(req,user='intranet')
     util = Laser.Util.web.impl(base)
 
     # split up campaign ID
@@ -97,17 +105,21 @@ def points(req, cid=None, extent=None):
         req.write('<pre><strong>Shell-Befehl:</strong>\n\n')
         req.write(' '.join(args))
 
-        # execute las2las command
+        # execute las2las command, log files and give feedback
         req.write('\n\nrunning command, please wait ...\n')
 
         proc = Popen(args, stdout=PIPE, stderr=PIPE)
         stdout, stderr = proc.communicate()
 
         # show lasfiles involved
+        files = []
+        files.append(args[-1])
         req.write('\n\nLAS-Dateien in %s:\n\n' % args[2])
         for fname in open(args[2]).readlines():
+            files.append(fname)
             req.write('%s' % fname)
         req.write('</pre>')
+        _log_files(req,dbh,base,files)
 
     # finish
     dbh.close()
@@ -118,7 +130,7 @@ def strips(req, cid=None, extent=None):
     """ get strips for campaign intersecting extent """
 
     # init application
-    (base,dbh,tpl) = Laser.base.impl().init(req)
+    (base,dbh,tpl) = Laser.base.impl().init(req,user='intranet')
     util = Laser.Util.web.impl(base)
 
     # split up campaign ID
@@ -131,13 +143,17 @@ def strips(req, cid=None, extent=None):
         # show errors
         req.write('ERROR: %s' % cmds)
     else:
-        # execute commands and give feedback
+        # execute commands, log files and give feedback
+        files = []
         req.write('<h3>Daten im Downloadbereich bereitgestellt</h3>')
         req.write('<pre><strong>Shell-Befehle:</strong>\n\n')
         for args in cmds:
                 req.write('%s\n' % ' '.join(args) )
                 proc = Popen(args, stdout=PIPE, stderr=PIPE)
                 stdout, stderr = proc.communicate()
+                files.append(args[2])
+
+        _log_files(req,dbh,base,files)
         req.write('</pre>')
 
     # finish
