516753bc1abdb4806654c9096c94e18d91684c19
Klaus Förster <openwebcc@github.com>
2016-11-25 14:10:21 +0100

    provide download result page with downloaded files and how to access them via samba

--- www/html/lidar/restricted/download.py
+++ www/html/lidar/restricted/download.py
@@ -21,13 +21,13 @@ APP_ROOT = '/data/lidar'
 
 def _log_files(req,dbh,base,files):
     """ log files downloaded by user """
-    dbh.execute("INSERT INTO lidar_log (user_id,files) VALUES (%s,%s)", (
+    dbh.execute("INSERT INTO lidar_log (user_id,files) VALUES (%s,%s) RETURNING id", (
         base.get_user(),files
     ))
+    return dbh.fetchone()['id']
 
 def index(req):
     """ provide empty startpage for now """
-
     req.content_type = "text/html"
     return "."
 
@@ -44,16 +44,22 @@ def lasfile(req, gid=None):
         ipath = '%s/las/%s' % (util.path_to_campaign(cid),row['fname'])
         opath = '%s/%s_%s' % (util.get_download_dir(),util.get_cid_as_prefix(cid),row['fname'])
 
-        # execute command, log files and give feedback
+        # execute command
         proc = Popen(['ln', '-s', ipath, opath], stdout=PIPE, stderr=PIPE)
         stdout, stderr = proc.communicate()
-        _log_files(req,dbh,base,[ipath])
-        req.write('<h3>Daten im Downloadbereich bereitgestellt</h3>')
-        req.write("<pre><strong>Shell-Befehl:</strong>\n\nln -s %s %s</pre><br>" % (ipath,opath))
+
+        # log file
+        last_id = _log_files(req,dbh,base,[ipath])
+
+        # fill template terms
+        tpl.add_term('APP_subdir',last_id)
+        tpl.add_term('APP_files',opath)
 
     # finish
     dbh.close()
-    raise apache.SERVER_RETURN, apache.OK
+    tpl.add_term('APP_root',APP_ROOT)
+    tpl.add_term('APP_user',base.get_user())
+    return tpl.resolve_template('/home/institut/www/html/data/lidar/restricted/templates/download.tpl')
 
 
 def trajectory(req, gid=None):
@@ -70,18 +76,26 @@ def trajectory(req, gid=None):
         opath = '%s/%s_%s.traj.txt' % (util.get_download_dir(),util.get_cid_as_prefix(cid),row['fname'])
 
         if os.path.exists(ipath):
-            # execute command, log files and give feedback
+            # execute command
             proc = Popen(['ln', '-s', ipath, opath], stdout=PIPE, stderr=PIPE)
             stdout, stderr = proc.communicate()
-            _log_files(req,dbh,base,[ipath])
-            req.write('<h3>Daten im Downloadbereich bereitgestellt</h3>')
-            req.write("<pre><strong>Shell-Befehl:</strong>\n\nln -s %s %s</pre><br>" % (ipath,opath))
+
+            # log file
+            last_id =_log_files(req,dbh,base,[ipath])
+
+            # fill template terms
+            tpl.add_term('APP_subdir',last_id)
+            tpl.add_term('APP_files',opath)
+
         else:
-            req.write('<h3>no trajectory file found</h3>')
+            dbh.close()
+            return '<h3>ERROR: no trajectory file found</h3>'
 
     # finish
     dbh.close()
-    raise apache.SERVER_RETURN, apache.OK
+    tpl.add_term('APP_root',APP_ROOT)
+    tpl.add_term('APP_user',base.get_user())
+    return tpl.resolve_template('/home/institut/www/html/data/lidar/restricted/templates/download.tpl')
 
 
 def points(req, cid=None, extent=None):
@@ -99,31 +113,34 @@ def points(req, cid=None, extent=None):
 
     if type(args) == str:
         # show errors
-        req.write('ERROR: %s' % args)
+        dbh.close()
+        return 'ERROR: %s' % args
     else:
-        req.write('<h3>Daten im Downloadbereich bereitgestellt</h3>')
-        req.write('<pre><strong>Shell-Befehl:</strong>\n\n')
-        req.write(' '.join(args))
+        # prepare file-listings
+        files = []
+        files_downloaded = [args[-1]]
 
-        # execute las2las command, log files and give feedback
-        req.write('\n\nrunning command, please wait ...\n')
-
+        # execute las2las command
         proc = Popen(args, stdout=PIPE, stderr=PIPE)
         stdout, stderr = proc.communicate()
 
         # show lasfiles involved
-        files = []
-        files.append(args[-1])
-        req.write('\n\nLAS-Dateien in %s:\n\n' % args[2])
         for fname in open(args[2]).readlines():
-            files.append(fname)
-            req.write('%s' % fname)
-        req.write('</pre>')
-        _log_files(req,dbh,base,files)
+            files.append(fname.rstrip())
+            files_downloaded.append('... mit Punkten von %s' % fname.rstrip())
+
+        # log files
+        last_id =_log_files(req,dbh,base,files)
+
+        # fill template terms
+        tpl.add_term('APP_subdir',last_id)
+        tpl.add_term('APP_files','\n'.join(files_downloaded))
 
     # finish
     dbh.close()
-    raise apache.SERVER_RETURN, apache.OK
+    tpl.add_term('APP_root',APP_ROOT)
+    tpl.add_term('APP_user',base.get_user())
+    return tpl.resolve_template('/home/institut/www/html/data/lidar/restricted/templates/download.tpl')
 
 
 def strips(req, cid=None, extent=None):
@@ -141,24 +158,33 @@ def strips(req, cid=None, extent=None):
 
     if type(cmds) == str:
         # show errors
-        req.write('ERROR: %s' % cmds)
+        dbh.close()
+        return 'ERROR: %s' % cmds
     else:
-        # execute commands, log files and give feedback
+        # prepare file-listings
         files = []
-        req.write('<h3>Daten im Downloadbereich bereitgestellt</h3>')
-        req.write('<pre><strong>Shell-Befehle:</strong>\n\n')
-        for args in cmds:
-                req.write('%s\n' % ' '.join(args) )
-                proc = Popen(args, stdout=PIPE, stderr=PIPE)
-                stdout, stderr = proc.communicate()
-                files.append(args[2])
+        files_downloaded = []
 
-        _log_files(req,dbh,base,files)
-        req.write('</pre>')
+        # execute commands
+        for args in cmds:
+            proc = Popen(args, stdout=PIPE, stderr=PIPE)
+            stdout, stderr = proc.communicate()
+
+            files.append(args[2])
+            files_downloaded.append(args[3])
+
+        # log files
+        last_id =_log_files(req,dbh,base,files)
+
+        # fill template terms
+        tpl.add_term('APP_subdir',last_id)
+        tpl.add_term('APP_files','\n'.join(files_downloaded))
 
     # finish
     dbh.close()
-    raise apache.SERVER_RETURN, apache.OK
+    tpl.add_term('APP_root',APP_ROOT)
+    tpl.add_term('APP_user',base.get_user())
+    return tpl.resolve_template('/home/institut/www/html/data/lidar/restricted/templates/download.tpl')
 
 def report(req, cid=None):
     """ get flight report for given campaign if any """
